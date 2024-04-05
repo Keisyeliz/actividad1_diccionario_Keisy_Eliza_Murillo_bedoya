c7289e470637c22c44247f5ed9c328c63fa0b129
Keisyeliz <158522128+Keisyeliz@users.noreply.github.com>
2024-04-05 11:46:10 -0500

    Update eje_diccionario.py

--- eje_diccionario.py
+++ eje_diccionario.py
@@ -3,44 +3,40 @@ list_pendiente = []
 
 import os
 sw = True
-
-def fnt_añadir(diccionario,codigo, nombres,contacto,correo,edad,estracto,sexo,direccion):
-    if codigo == '' or nombres == ''  or contacto == '' or correo == '' or edad == '' or estracto=='' or sexo =='' or direccion =='':
+def fnt_añadir(diccionario, codigo, nombres, contacto, correo, edad, estrato, sexo, direccion):
+    if not all([codigo, nombres, contacto, correo, edad, estrato, sexo, direccion]):
         enter = input('Debe diligenciar toda la información solicitada <ENTER>')
     else:
-        diccionario[codigo] = {'Nombre': nombres, 'Contacto':contacto,'Correo':correo, 'Edad':edad, 'Estracto':estracto,'Sexo':sexo,'Direccion':direccion}
+        diccionario[codigo] = {'Nombre': nombres, 'Contacto': contacto, 'Correo': correo, 'Edad': edad, 'Estrato': estrato, 'Sexo': sexo, 'Direccion': direccion}
         enter = input(f'\nLa persona {nombres} ha sido registrada con éxito <ENTER>')
-
 def fnt_selector(op):
     if op == '1':
         os.system('cls')
-        codigo = input('Ingrese su codigo:')
-        nombres = input('Nombres completos:  ')
-        contacto = input('Número de contacto:   ')
-        correo = input('Ingrese su correo:  ')
+        codigo = input('Ingrese su codigo: ')
+        nombres = input('Nombres completos: ')
+        contacto = input('Número de contacto:  ')
+        correo = input('Ingrese su correo: ')
         direccion = input('Ingrese su dirección: ')
         sexo = input('Sexo (M/F):  ')
-        if sexo == 'M':
-            edad = int(input('Ingrese su edad: '))
-            if (edad >= 15 and edad <= 25):
-                estracto = input('--Estracto--\n\n1 .1\n2. 2\n-> ')
-            else:
-                print ('No cuentas con la edad suficiente para realizar el registro <ENTER>')
-                list_pendiente.append([contacto])
-        elif sexo == 'F':
-            edad = int(input('Ingrese su edad: '))
-            if (edad >= 20 and edad <= 35):
-                estracto = input('--Estracto--\n\n1 .1\n2. 2\n3. 3\n4. 4\n -> ')
-            else:
-                print ('No cuentas con la edad suficiente para realizar el registro <ENTER>')
-                list_pendiente.append([contacto])        
-        fnt_añadir(registro_diccionario,codigo, nombres,contacto,correo,edad,estracto,sexo,direccion)
-
-
+        edad = int(input('ingrese su Edad: '))
+        estrato = int(input('Ingrese su estrato: '))
+        
+        if (sexo == 'M' and 15 <= edad <= 25 and estrato in (1, 2)) or (sexo == 'F' and 20 <= edad <= 35 and estrato in (1, 2, 3, 4)):
+            fnt_añadir(registro_diccionario, codigo, nombres, contacto, correo, edad, estrato, sexo, direccion)
+        else:
+            print('No cumple con la edad necesaria para realizar el regitro, pero agregaremos su contacto a la lista "Pendiente".')
+            list_pendiente.append(contacto)
+        enter = input('\nPresione <ENTER> para continuar...\n')
+def fnt_mostrar_pendiente():
+    os.system('cls')
+    print('\nLista de contactos pendientes:\n')
+    for contacto in list_pendiente:
+        print(f'- {contacto}')
+    enter = input('\nPresione <ENTER> para continuar')
 
 while sw == True:
     os.system('cls')
-    opcion = input('1. Registrar\n2. Mostrar\n3. Salir\n- >  ')
+    opcion = input('1. Registrar\n2. Mostrar\n3. Mostrar pendientes\n4. Salir\n- >  ')
     if opcion == "1":
         fnt_selector(opcion)
     elif opcion == '2':
@@ -48,8 +44,8 @@ while sw == True:
         print('\nCantidad de registros: ',len(registro_diccionario),'\n')
         for clave, valor in registro_diccionario.items():
             print(f"{clave}: {valor}")
-        for clave, valor in list_pendiente.items():
-            print(list_pendiente)
-        enter = input('\n\nPresione <ENTER> para continuar')
+        enter = input('\nPresione <ENTER> para continuar')
     elif opcion == '3':
+        fnt_mostrar_pendiente()
+    elif opcion == '4':
         sw = False
